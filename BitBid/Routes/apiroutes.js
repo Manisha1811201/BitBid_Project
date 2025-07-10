fcbeb98b1d1482852daa6d46056b08464a939d4a
Manisha1811201 <manishalambha8@gmail.com>
2025-07-10 14:15:15 +0530

    userlogin api and in customer folder(getall,getsingle and update api's ) there routing and testing

--- BitBid/Routes/apiroutes.js
+++ BitBid/Routes/apiroutes.js
@@ -6,48 +6,58 @@ const queryController=require("../Server/Query/queryController");
 const rateController=require("../Server/Rating/rateController");
 const bidController=require("../Server/Bid/bidController");
 const taskController=require("../Server/Task/taskController");
-const custController=require("../Server/Customer/custController")
+const custController=require("../Server/Customer/custController");
+const userController=require("../Server/User/userController");
 
+//techController routing
 router.post("/tech/add",techController.add)
 router.post("/tech/getall",techController.getall)
 router.post("/tech/getsingle",techController.getsingleData)
 router.post("/tech/delete",techController.deletedata)
 router.post("/tech/update",techController.updateData)
 
-
+//postController routing
 router.post("/post/add",postController.add)
 router.post("/post/getall",postController.getall)
 router.post("/post/getsingle",postController.getsingleData)
 router.post("/post/delete",postController.deleteData)
 router.post("/post/update",postController.updateData)
 
-
+//queryController routing
 router.post("/query/add",queryController.add)
 router.post("/query/getall",queryController.getall)
 router.post("/query/getsingle",queryController.getsingleData)
 router.post("/query/delete",queryController.deletedata)
 router.post("/query/update",queryController.updateData)
 
-
+//rateController routing
 router.post("/rate/add",rateController.add)
 router.post("/rate/getall",rateController.getall)
 router.post("/rate/getsingle",rateController.getsingleData)
 router.post("/rate/delete",rateController.deletedata)
 router.post("/rate/update",rateController.updateData)
 
-
+//bitController routing
 router.post("/bid/add",bidController.add)
 router.post("/bid/getall",bidController.getall)
 router.post("/bid/getsingle",bidController.getsingleData)
 router.post("/bid/delete",bidController.deletedata)
 router.post("/bid/update",bidController.updateData)
 
+//taskController routing
 router.post("/task/add",taskController.add)
 router.post("/task/getall",taskController.getall)
 router.post("/task/getsingle",taskController.getsingleData)
 router.post("/task/delete",taskController.deleteData)
 router.post("/task/update",taskController.updateData)
 
+//customerControler routing
+router.post("/cust/getall",custController.getall)
+router.post("/cust/getsingle",custController.getsingleData)
+router.post("/cust/update",custController.updateData)
 router.post("/cust/register",custController.register)
+
+//userController routing
+router.post("/user/login",userController.login)
 
 module.exports=router
